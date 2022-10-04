b3c5924c7444cf7d362fb3900346e737ea0ca383
Przemysław Lehr <plehr7@gmail.com>
2022-10-04 14:46:25 +0200

    weather component with exact weather information

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import { useState } from "react";
+import {useState} from "react";
 
 import SearchBar from "./components/SearchBar";
 import Weather from "./components/Weather";
 import WeatherForecastChart from "./components/WeatherForecastChart";
+import {getWeatherFromApi} from "./utils/api";
 
-import "./styles/main.scss";
+import './styles/main.scss';
+
 
 function App() {
-    const [searchValue, setSearchValue] = useState("");
+    const [searchValue, setSearchValue] = useState('');
+    const [forecastData, setForecastData] = useState({});
 
-    function handleSearch() {
+    async function handleSearch(){
+        const data = await getWeatherFromApi(searchValue);
+        setForecastData(data);
         setSearchValue("");
     }
 
     return (
         <main className="App">
-            <SearchBar
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-                handleSearch={handleSearch}
-            />
-            <Weather />
-            <WeatherForecastChart />
+            <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} handleSearch={handleSearch}/>
+            {!!Object.keys(forecastData).length && (
+                <Weather currentWeather={{...forecastData.current, ...forecastData.location}}/>
+            )}
+            <WeatherForecastChart/>
         </main>
     );
 }
---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+const API_KEY = 'cbcaf9f0dfac4765852112253220410';
+const BASE_URL = 'http://api.weatherapi.com/v1/forecast.json';
+
+
+export async function getWeatherFromApi (city) {
+const config = {
+    key: API_KEY,
+    q: city,
+    days: 10,
+    aqi: "no",
+    alerts: "no",
+}
+
+    const response = await fetch(`${BASE_URL}?${new URLSearchParams(config)}`)
+
+    return response.json()
+}
